2570d8aa8dc50939cc23b8d3296ec95d610b9678
Arjun K <arjunk03@users.noreply.github.com>
2025-10-14 11:24:33 +0530

    added solution

--- Rep-1/array/matrix/spiral_matrix_54.py
+++ Rep-1/array/matrix/spiral_matrix_54.py
@@ -2,9 +2,35 @@ a = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
 
 n = len(a)
 LEFT = TOP = 0
-BOTTOM = RIGHT = n - 1
+BOTTOM , RIGHT = n - 1, len(a[0]) - 1
 
 res = []
-# LEFT to RIGHT
-for i in range(RIGHT+1):
-    res
+
+while LEFT <= RIGHT and TOP <= BOTTOM:
+    # LEFT to RIGHT
+    for i in range(LEFT, RIGHT+1):
+        res.append(a[TOP][i])
+    
+    TOP += 1
+
+    #RIGHT to BOTTOM
+    for i in range(TOP, BOTTOM+1):
+        res.append(a[i][RIGHT])
+    
+    RIGHT -= 1
+
+    if TOP <= BOTTOM:
+        #RIGHT to LEFT
+        for i in range(RIGHT,LEFT-1, -1):
+            res.append(a[BOTTOM][i])
+        
+        BOTTOM -= 1
+
+    if LEFT <= RIGHT:
+        #BOTTOM to TOP
+        for i in range(BOTTOM, TOP-1, -1):
+            res.append(a[i][LEFT])
+        
+        LEFT += 1
+
+print(res)
